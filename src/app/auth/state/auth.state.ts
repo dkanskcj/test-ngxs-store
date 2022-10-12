b91ff19669f2b436adcecd169ea79bff197eb925
dkanskcj <dkanskcj@likelion.org>
2022-10-12 18:02:03 +0900

    login test

--- src/app/auth/state/auth.state.ts
+++ src/app/auth/state/auth.state.ts
@@ -3,6 +3,8 @@ import { Action, State, StateContext, StateToken } from "@ngxs/store";
 import { AuthActions } from "./auth.actions";
 
 export interface AuthModel {
+    name: string;
+    password: string;
     isLoggedIn: boolean;
 }
 
@@ -11,16 +13,25 @@ export const AUTH_STATE_TOKEN = new StateToken<AuthModel>('auth');
 @State<AuthModel>({
     name: AUTH_STATE_TOKEN,
     defaults: {
+        name: '',
+        password: '',
         isLoggedIn: false
     }
 })
 @Injectable()
 export class AuthState{
+
+    constructor(
+
+    ){}
+
     @Action(AuthActions.Login)
     login(ctx: StateContext<AuthModel>){
         const state = ctx.getState();
         ctx.setState({
             ...state,
+            name: '',
+            password: '',
             isLoggedIn: true
         });
     }
@@ -30,6 +41,8 @@ export class AuthState{
         const state = ctx.getState();
         ctx.setState({
             ...state,
+            name: '',
+            password: '',
             isLoggedIn: false
         });
     }
